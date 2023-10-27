0ab656f11d945e3afc0f73785378f50658672b72
NhanTai0512 <105573251+NhanTai0512@users.noreply.github.com>
2023-10-27 22:14:22 +0700

    Update

--- selenium-webdriver-java-master/src/test/java/test/comparePriceForm.java
+++ selenium-webdriver-java-master/src/test/java/test/comparePriceForm.java
@@ -20,7 +20,7 @@ public class comparePriceForm {
         try {
 
             driver.get("http://live.techpanda.org/");
-            driver.findElement(By.xpath("//ol[@class='nav-primary']//li[@class='level0 nav-1 first']")).click();
+            driver.findElement(By.xpath("//ol[@class='nav-primary']//li[@class='level0 nav-1 first active']")).click();
 
 
             List<WebElement> liElements = driver.findElements(new By.ByClassName("product-info"));
